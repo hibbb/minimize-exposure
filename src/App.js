36b0d1699a96748d59926e8e6fb25420d89b7adf
liubenben <qieda@163.com>
2023-04-25 14:03:44 +0800

    0425-2

--- src/App.js
+++ src/App.js
@@ -1,44 +1,77 @@
 import { publicClient, walletClient } from './utils/client'
 import { namehash, normalize } from 'viem/ens'
 import { abi } from './utils/abi'
+import { createIdentity } from 'eth-crypto/src/create-identity'
+import { encryptWithPublicKey, decryptWithPrivateKey } from 'eth-crypto'
+import { useState } from 'react'
 
-async function getSomething() {
+const identity = createIdentity();
+
+
+async function saveInfo(infoKey, infoValue) {
+
   const [account] = await walletClient.requestAddresses() 
-  console.log(account)
 
   const name = await publicClient.getEnsName({
     address: account
   })
-  console.log(name)
 
   const node = namehash(name)
-  console.log(node)
+
+  const resolverAddress = await publicClient.getEnsResolver({
+    name: normalize(name),
+  })
+
+  const encryptedValue = await encryptWithPublicKey(identity.publicKey, infoValue)
 
   const { request } = await publicClient.simulateContract({
     account,
-    address: '0xd7a4F6473f32aC2Af804B3686AE8F1932bC35750',
+    address: resolverAddress,
     abi: abi,
     functionName: 'setText',
-    args: [node, "test-key", "test-value"]
+    args: [node, infoKey, encryptedValue]
   })
 
   await walletClient.writeContract(request)  
-  console.log("ok")
+}
+
+async function test() {
   
+  const resolverAddress = await publicClient.getEnsResolver({
+    name: normalize("ceshi2.eth"),
+  })
+  console.log(resolverAddress)
+
+  // const decrypted = await decryptWithPrivateKey(identity.privateKey, encrypted)
+  // console.log(decrypted)
+  // console.log(msg === decrypted)
 }
 
 
 
 function App() {
+  const [infoValue, setInfoValue] = useState("");
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setInfoValue(value);
+  };
+
+
   return (
     <div className="app">
       <header className="app-header">
         This is Header
       </header>
       <div className="app-content">
-        <input id="info" placeholder="Type a piece of information here" />
-        <button onClick={() => getSomething()}>
+        <input id="info" onChange={handleChange} placeholder="Type a piece of information here" />
+        <br />
+        <button onClick={() => saveInfo("test2-key", infoValue)}>
           Then Click This Button
+        </button>
+        <br />
+        <button onClick={() => test()}>
+          Test Button
         </button>
       </div>
     </div>
