42fb6cb5b51c51a6997d5323ef67c45c99394ee2
hibbb <lbbfor@gmail.com>
2023-04-25 11:31:18 +0800

    0425-1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import { publicClient, walletClient } from './utils/client'
+import { namehash, normalize } from 'viem/ens'
+import { abi } from './utils/abi'
 
 async function getSomething() {
-  const [accounts] = await walletClient.requestAddresses() 
-  console.log(accounts)
+  const [account] = await walletClient.requestAddresses() 
+  console.log(account)
 
   const name = await publicClient.getEnsName({
-    address: accounts
+    address: account
   })
   console.log(name)
+
+  const node = namehash(name)
+  console.log(node)
+
+  const { request } = await publicClient.simulateContract({
+    account,
+    address: '0xd7a4F6473f32aC2Af804B3686AE8F1932bC35750',
+    abi: abi,
+    functionName: 'setText',
+    args: [node, "test-key", "test-value"]
+  })
+
+  await walletClient.writeContract(request)  
+  console.log("ok")
   
 }
 
